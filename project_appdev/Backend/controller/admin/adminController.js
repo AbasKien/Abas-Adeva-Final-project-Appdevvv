9eb31c57399bc4734f65ee88e38cd3adc6d59bda
AbasKien <abaskien@gmail.com>
2024-12-01 21:16:46 +0800

    Delete project_appdev directory

--- project_appdev/Backend/controller/admin/adminController.js
+++
@@ -1,5 +0,0 @@
-const upload = require('../../config/upload'); // Import the upload middleware
-
-module.exports = {
-    upload: upload.single('image') // Middleware to handle single image upload
-};
